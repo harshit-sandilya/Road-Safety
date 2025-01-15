b632fa1b5cbfc49434782f680de00c798cf5bd6b
Harshit-Sandilya <2021ucp1382@mnit.ac.in>
2025-01-16 01:23:09 +0530

    piper has no attribute load_model

--- utils/text_to_speech.py
+++ utils/text_to_speech.py
@@ -3,7 +3,7 @@ import piper
 
 class TextToSpeech:
     def __init__(self, language="en"):
-        self.model = piper.load_model(language)
+        self.model = piper.load(language)
 
     def speak(self, text):
         audio = self.model.synthesize(text)
