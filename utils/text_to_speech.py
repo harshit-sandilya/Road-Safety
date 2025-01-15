43685f6d0447a31395b07cf4f08540047be2a33c
Harshit-Sandilya <2021ucp1382@mnit.ac.in>
2025-01-16 01:51:55 +0530

    piper -> PiperVoice

--- utils/text_to_speech.py
+++ utils/text_to_speech.py
@@ -1,10 +1,13 @@
-import piper
+from piper import PiperVoice
+import sys
 
 
 class TextToSpeech:
     def __init__(self, language="en"):
-        self.model = piper.Model(language)
+        self.model = PiperVoice.load("data/en_US-amy-low.onnx")
 
     def speak(self, text):
-        audio = self.model.synthesize(text)
-        audio.play()
+        audio_stream = self.model.synthesize_stream_raw(text)
+        for audio_bytes in audio_stream:
+            sys.stdout.buffer.write(audio_bytes)
+            sys.stdout.buffer.flush()
