e0d5b2e3317b5ebe106c7a6836e0752b58a672d8
Harshit Sandilya <harshitsandilya@gmail.com>
2025-02-28 20:24:20 +0000

    Hindi tts error handling

--- main.py
+++ main.py
@@ -13,15 +13,23 @@ tts = TextToSpeech(language)
 os.system("cls" if os.name == "nt" else "clear")
 
 while True:
-    tts.speak("Please ask your question")
-    print("Please ask your question")
+    if language != "hi":
+        tts.speak("Please ask your question")
+        print("Please ask your question")
+    else:
+        print(handler.translator.translate_en("Please ask your question"))
     question = stt.start_listening()
     print(f"Question: {question}")
     answer = handler.process_question(question)
     print(f"Answer: {answer}")
-    tts.speak(answer)
-    tts.speak("Do you have any more questions?")
-    print("Do you have any more questions?")
+    if language != "hi":
+        tts.speak(answer)
+        tts.speak("Do you have any more questions?")
+        print("Do you have any more questions?")
+    else:
+        print(handler.translator.translate_en(("Do you have any more questions?"))
     more_questions = stt.start_listening()
+    if language == "hi":
+        more_questions = handler.translator.translate_hi(more_questions)
     if more_questions.lower() == "no":
         break
